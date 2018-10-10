6a2130ba5fe30aa1fa4197d45057babed473ad07
christianbtn2 <christianbtn2@gmail.com>
2018-10-09 20:07:11 -0600

    Cuestiones esteticas y arreglos

--- app/src/main/java/com/example/luisp/eddii_lab_02/Menu.java
+++ app/src/main/java/com/example/luisp/eddii_lab_02/Menu.java
@@ -1,13 +1,32 @@
 package com.example.luisp.eddii_lab_02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class Menu extends AppCompatActivity {
+
+    ImageButton btnZigZag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnZigZag = (ImageButton)findViewById(R.id.btnZigZag);
+
+
+        btnZigZag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btnZigZag = new Intent(Menu.this,Cifrados.class);
+                startActivity(btnZigZag);
+            }
+        });
+
+
     }
 }
